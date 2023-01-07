67c1b1e43a02263f33e3883dcf7a6ec35d79a927
Kirilenko <>
2023-01-07 16:52:27 +0400

    feat: task 'palindrome-number' - better solution than the previous ones

--- src/palindrome-number/index.ts
+++ src/palindrome-number/index.ts
@@ -3,9 +3,30 @@
 export type Args = [number]
 
 export const isPalindrome0 = (...[x]: Args): boolean => {
+  if (x < 0 || (x && x % 10 === 0)) {
+    return false
+  }
+
   const xString = x.toString()
   return xString === xString.split('').reverse().join('')
 }
 
 export const isPalindrome1 = (...[x]: Args): boolean =>
-  x === +[...x.toString()].reverse().join('')
+  x >= 0 && !(x && x % 10 === 0) && x === +[...x.toString()].reverse().join('')
+
+export const isPalindromeBetter = (...[x]: Args): boolean => {
+  if (x < 0 || (x && x % 10 === 0)) {
+    return false
+  }
+
+  let reverse = 0
+  let copy = x
+
+  while (copy > 0) {
+    const digit = copy % 10
+    reverse = reverse * 10 + digit
+    copy = Math.floor(copy / 10)
+  }
+
+  return reverse == x
+}
