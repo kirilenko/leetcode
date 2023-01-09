c4fd196ed4e853b7078c721fbd60811fa8eeb928
Kirilenko <>
2023-01-09 15:25:41 +0400

    feat: task 'valid-palindrome'

---
+++ src/palindrome/index.ts
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+export type Args = [string]
+
+// It's faster than palindrome1, ..2 for long strings:
+export const isPalindrome0 = (...[rawString]: Args): boolean => {
+  const string = rawString.replace(/[^0-9a-z]/gi, '').toLowerCase()
+  const length = string.length
+  const lengthHalf = Math.floor(length / 2)
+  for (let i = 0; i <= lengthHalf; i++) {
+    if (string[i] !== string[length - 1 - i]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+export const isPalindrome1 = (...[rawString]: Args): boolean => {
+  const string = rawString.replace(/[^0-9a-z]/gi, '').toLowerCase()
+  return string === string.split('').reverse().join('')
+}
+
+// It's slower than isPalindrome1 for long strings, but faster for short strings:
+export const isPalindrome2 = (...[rawString]: Args): boolean => {
+  const string = rawString.replace(/[^0-9a-z]/gi, '').toLowerCase()
+  return string === [...string].reverse().join('')
+}
+
+export const isPalindromeBetter = (...[rawString]: Args): boolean => {
+  const string = rawString.replace(/[^0-9a-z]/gi, '').toLowerCase()
+  let i = 0
+  let j = string.length - 1
+  while (i < j) {
+    if (string[i] !== string[j]) {
+      return false
+    }
+
+    i++
+    j--
+  }
+
+  return true
+}
