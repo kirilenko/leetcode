bcb1dc871014fe3dcb340347511628f77f9ee3fd
Kirilenko <>
2023-01-10 18:31:16 +0400

    feat: task 'longest-palindromic-substring' - better solution than the previous ones

--- src/longest-palindromic-substring/index.ts
+++ src/longest-palindromic-substring/index.ts
@@ -1,17 +1,23 @@
+// https://leetcode.com/problems/longest-palindromic-substring/
+
 export type Args = [string]
 
 export const isPalindrome = (s: string): boolean => {
-  const length = s.length
-  for (let i = 0; i < length / 2; i++) {
-    if (s[i] !== s[length - 1 - i]) {
+  let i = 0
+  let j = s.length - 1
+  while (i < j) {
+    if (s[i] !== s[j]) {
       return false
     }
+
+    i++
+    j--
   }
 
   return true
 }
 
-// Complexity: O(n2)
+// Complexity: O(n3)
 export const longestPalindrome0 = (...[s]: Args): string => {
   let res = ''
   const length = s.length
@@ -28,7 +34,7 @@ export const longestPalindrome0 = (...[s]: Args): string => {
   return res
 }
 
-// Complexity: O(n2)
+// Complexity: O(n3)
 export const longestPalindrome1 = (...[s]: Args): string => {
   let res = ''
   const length = s.length
@@ -43,4 +49,37 @@ export const longestPalindrome1 = (...[s]: Args): string => {
   }
 
   return res
+}
+
+// Complexity: O(n2)
+export const longestPalindrome2 = (...[s]: Args): string => {
+  const expandAroundCenter = (
+    str: string,
+    left: number,
+    right: number,
+  ): number => {
+    const strLength = str.length
+    while (left >= 0 && right < strLength && str[left] === str[right]) {
+      left--
+      right++
+    }
+
+    return right - left - 1
+  }
+
+  const length = s.length
+  let start = 0
+  let end = 0
+
+  for (let i = 0; i <= length; i++) {
+    const len1 = expandAroundCenter(s, i, i) // search palindrome w/ odd-numbered length
+    const len2 = expandAroundCenter(s, i, i + 1) // ...even-numbered length
+    const len = Math.max(len1, len2)
+    if (len > end - start) {
+      start = i - Math.floor((len - 1) / 2)
+      end = i + Math.floor(len / 2)
+    }
+  }
+
+  return s.substring(start, end + 1)
 }
