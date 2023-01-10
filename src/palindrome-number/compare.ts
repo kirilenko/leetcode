b4eff52041212addbdf5df9f5dd92d449b2defd9
Kirilenko <>
2023-01-10 22:46:44 +0400

    feat: task 'palindrome-number' - better solution than the previous ones

--- src/palindrome-number/compare.ts
+++ src/palindrome-number/compare.ts
@@ -1,11 +1,18 @@
 import comparator, { Case } from '../_common/comparator'
-import { Args, isPalindrome0, isPalindrome1, isPalindromeBetter } from '.'
+import {
+  Args,
+  isPalindrome0,
+  isPalindrome1,
+  isPalindrome2,
+  isPalindrome3,
+} from '.'
 
 const cases: Case<Args>[] = [
   { args: [121], description: 'short number and palindrome' },
   { args: [-121], description: 'negative number' },
   { args: [123], description: 'short number and not palindrome' },
   { args: [0], description: 'zero' },
+  { args: [5], description: 'less then ten' },
   {
     args: [2147447412],
     description: 'long number less than 2^31 - 1 and palindrome',
@@ -14,5 +21,5 @@ const cases: Case<Args>[] = [
 
 comparator<Args>({
   cases,
-  fns: [isPalindrome0, isPalindrome1, isPalindromeBetter],
+  fns: [isPalindrome0, isPalindrome1, isPalindrome2, isPalindrome3],
 })
--- src/palindrome-number/index.ts
+++ src/palindrome-number/index.ts
@@ -14,9 +14,13 @@ export const isPalindrome0 = (...[x]: Args): boolean => {
 export const isPalindrome1 = (...[x]: Args): boolean =>
   x >= 0 && !(x && x % 10 === 0) && x === +[...x.toString()].reverse().join('')
 
-export const isPalindromeBetter = (...[x]: Args): boolean => {
+export const isPalindrome2 = (...[x]: Args): boolean => {
   if (x < 0 || (x && x % 10 === 0)) {
     return false
+  }
+
+  if (x < 10) {
+    return true
   }
 
   let reverse = 0
@@ -28,5 +32,24 @@ export const isPalindromeBetter = (...[x]: Args): boolean => {
     copy = Math.floor(copy / 10)
   }
 
-  return reverse == x
+  return reverse === x
+}
+
+// Complexity O(log10(n))
+export const isPalindrome3 = (...[x]: Args): boolean => {
+  if (x < 0 || (x && x % 10 === 0)) {
+    return false
+  }
+
+  if (x < 10) {
+    return true
+  }
+
+  let reverse = 0
+  while (x > reverse) {
+    reverse = reverse * 10 + (x % 10)
+    x = Math.floor(x / 10)
+  }
+
+  return reverse === x || Math.floor(reverse / 10) === x
 }
