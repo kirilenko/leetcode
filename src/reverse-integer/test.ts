947f4fd1b320a33adb9f10d38cd12942e2277e24
Kirilenko <>
2023-01-17 11:32:14 +0400

    feat: task 'reverse-integer'

---
+++ src/reverse-integer/test.ts
@@ -0,0 +1,50 @@
+import { Args, fns, Result } from '.'
+
+type Case = {
+  expected: Result
+  id: string
+  props: Args
+}
+
+const cases: Case[] = [
+  {
+    expected: 321,
+    id: '0',
+    props: [123],
+  },
+  {
+    expected: -321,
+    id: '1',
+    props: [-123],
+  },
+  {
+    expected: 21,
+    id: '2',
+    props: [120],
+  },
+  {
+    expected: 0,
+    id: '3',
+    props: [0],
+  },
+  {
+    expected: 0,
+    id: '4',
+    props: [1534236469],
+  },
+  {
+    expected: 9,
+    id: '5',
+    props: [900000],
+  },
+]
+
+fns.forEach((fn) => {
+  describe(`reverse-integer.${fn.name}`, function () {
+    cases.forEach(({ expected, id, props }) => {
+      it(`should pass case ${id}`, function () {
+        expect(fn(...props)).toEqual(expected)
+      })
+    })
+  })
+})
